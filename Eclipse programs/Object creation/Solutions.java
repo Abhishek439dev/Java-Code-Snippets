982c4a768663fc22870a6b57a72a80aed1e62e15
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-07-16 16:21:17 +0530

    Update Solutions.java

--- Eclipse programs/Object creation/Solutions.java
+++ Eclipse programs/Object creation/Solutions.java
@@ -2,11 +2,13 @@ package com;
 
 class Solutions {
   public static void main(String[] args) {
-	Employee e1 = new Employee();
+	 //Object-creation or instantiation.
+	Employee e1 = new Employee();              
 	Employee e2 = new Employee();
 	Employee e3 = new Employee();
-	
-	e1.id = 1;
+
+	 //Initialization of the variables to new values for each object.
+	e1.id = 1;                                
 	e1.name = "Abhishek";
 	e1.salary = 50000;
 	
@@ -17,7 +19,8 @@ class Solutions {
 	e3.id = 3;
 	e3.name = "Ram";
 	e3.salary = 60000;
-	
+
+	  //Display of the outputs.
 	System.out.println("Employee with id "+e1.id+" is "+e1.name+" and has a salary of "+e1.salary);
 	System.out.println("Employee with id "+e2.id+" is "+e2.name+" and has a salary of "+e2.salary);
 	System.out.println("Employee with id "+e3.id+" is "+e3.name+" and has a salary of "+e3.salary);
