53276dc25f0c6518d17c24d5f1f7fc15a829c2ce
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-07-18 14:06:17 +0530

    Add files via upload

---
+++ largerofAll.java
@@ -0,0 +1,25 @@
+ class largerofAll{
+
+public static void main (String[] args){
+	int a = 2;
+	int b = 16;
+	int c = 80;
+
+	if(a>b){
+	   if(a>c){
+	     System.out.println("a is largest");
+		 }
+	   else{
+	     System.out.println("c is largest");
+	      }
+	}
+	else if(b>c){
+	     System.out.println("b is largest");
+		}
+	else{
+	     System.out.println("c is largest");
+	
+  	}
+
+ }
+}
