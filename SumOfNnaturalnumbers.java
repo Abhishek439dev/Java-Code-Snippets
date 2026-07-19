c20ccec4d02ead98bd7147f6495d37f39fc0db68
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-07-19 16:51:47 +0530

    Update SumOfNnaturalnumbers.java

--- SumOfNnaturalnumbers.java
+++ SumOfNnaturalnumbers.java
@@ -1,11 +1,12 @@
 class SumOfNnaturalnumbers {
     public static void main(String[] args) {
-        int i =5;
-        int sum = 0;
+        int i =5;     //value until which we have to count.
+        int sum = 0;  //variable to store the sum of all values.
 
+        //for loop to count from 1 to the value of 'i'.
         for(int a = 1; a<=i; a++){
-            sum = sum+a;
+            sum += a;  //sum = sum+a
         }
-        System.out.println(sum);
+        System.out.println(sum);  //result printing.
     }
 }
