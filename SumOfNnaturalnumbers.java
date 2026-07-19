5d9f5f162df4c3c4a5604e1454ba7ac2f869d8ad
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-07-19 16:49:15 +0530

    Add files via upload

---
+++ SumOfNnaturalnumbers.java
@@ -0,0 +1,11 @@
+class SumOfNnaturalnumbers {
+    public static void main(String[] args) {
+        int i =5;
+        int sum = 0;
+
+        for(int a = 1; a<=i; a++){
+            sum = sum+a;
+        }
+        System.out.println(sum);
+    }
+}
