7f1f19329a675183c679b8d13b1accf062fc9198
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-07-19 16:45:19 +0530

    Update Test.java

--- Eclipse programs/Scanner function/Test.java
+++ Eclipse programs/Scanner function/Test.java
@@ -1,22 +1,23 @@
 package com;
 
-import java.util.Scanner;
+import java.util.Scanner; //This library is important to import in order to use the Scanner function.
 
 public class Test {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);  //Scanner function is used after its object creation.
 		
-;		ScannerFunction sF = new ScannerFunction();
-		
-		for(int i=1; i<=5; i++) {
+;		ScannerFunction sF = new ScannerFunction();  //This is an object of the class ScannerFunction to use the method for checing number.
+
+		//This is a for loop for repeating the same process of accepting new user inputs and checking them for 5 times.
+		for(int i=1; i<=5; i++) {                    
 			System.out.println("Enter a number");
 			int a = sc.nextInt();
 			
 			sF.checkPositiveOrNegative(a);
 			System.out.println("_________________");
 		}
-		sc.close();
+		sc.close();      //This close() method closes the scanner function which is necessary to prevent resource leaks and fee up the resources.
 	}
 	
 	
