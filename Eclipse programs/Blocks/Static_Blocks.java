2f945c7f4074cc7769e9c0d3a306093a603c98ce
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-07-21 16:49:14 +0530

    Update Static_Blocks.java

--- Eclipse programs/Blocks/Static_Blocks.java
+++ Eclipse programs/Blocks/Static_Blocks.java
@@ -8,18 +8,18 @@ public class Static_Blocks {
 	static {
 		System.out.println("In static block 1");
 		age = 20;  //Initialized the value of age to 20.
-		System.out.println(age);
+		System.out.println(age);   //20
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("In main method");
-		System.out.println(age);
+		System.out.println(age);   //30
 	}
 	
 	static {
 		System.out.println("In static block 2");
 		age = 30;  //Initialized the value of age to 30.
-		System.out.println(age);
+		System.out.println(age);  //30
 	}
 }
 
